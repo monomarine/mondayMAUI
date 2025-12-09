4b04bd180e5547bfaa578b900c85567ac91fc63d
maurmni <ylavartalo@outlook.com>
2025-12-09 17:10:31 +0500

    сделано здаание

--- MauiApp4/DTO/ContactDto.cs
+++ MauiApp4/DTO/ContactDto.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
-namespace MauiApp4.DTO
+public class ContactDto
 {
-    public class ContactDto
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Phone { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
 
-        public string FullName => FirstName + " " + LastName;
-    }
+    [JsonPropertyName("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("phone")]
+    public string Phone { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+
+    public string FullName => $"{FirstName} {LastName}";
 }
